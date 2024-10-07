d501a374553c941ea72fa387906158a6d50e7167
paul-was-here <paul.g.kullmann@gmail.com>
2024-10-07 12:27:05 -0400

    cleaned up code

--- Thermal Switch 3D.py
+++ Thermal Switch 3D.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 from mpl_toolkits import mplot3d
 from matplotlib import cm
 
-#Numerical Values
+#Constant Values
 pi = math.pi
 dT = 85
 Ea = 10000
@@ -17,25 +17,24 @@ ts = 1/16
 ws = 1/8
 L = 4
 
-ta = np.linspace(0.04,0.05,500)
-wa = (dT*(alphaa-alphas)-(pi**2)*(ta**2)/3/(L**2))*((6*(L**2)*Es*ts*ws)/((pi**2)*Ea*(ta**3)))
-ylim = ta
-area = ta*wa
+#Functions
+def plotting(x,y,z,colors):
+    ax = plt.axes(projection='3d')
+    ax.set_ylim(0,0.7)
+    ax.set_xlim(0.04,0.05)
+    ax.set_zlim(0,0.04)
+    ax.set_ylabel("wₐ (in)")
+    ax.set_xlabel("tₐ (in)")
+    ax.set_zlabel('Area, (in²)')
+    ax.scatter3D(x,y,z, c=colors, cmap='RdYlGn')
+    plt.show()
+def allValues():
+    x = np.linspace(0.04,0.05,500)
+    y = (dT*(alphaa-alphas)-(pi**2)*(x**2)/3/(L**2))*((6*(L**2)*Es*ts*ws)/((pi**2)*Ea*(x**3)))
+    area = x*y
+    colors = -area
+    return(x,y,area,colors)
 
-colors = -area
-
-
-ax = plt.axes(projection='3d')
-ax.set_ylim(0,0.7)
-ax.set_xlim(0.04,0.05)
-ax.set_zlim(0,0.04)
-ax.set_ylabel("wₐ (in)")
-ax.set_xlabel("tₐ (in)")
-ax.set_zlabel('Area, (in²)')
-ax.scatter3D(ta,wa,area, c=colors, cmap='RdYlGn')
-
-
-plt.show()
-
-
-exit()
+#Main
+ta,wa,area,colors = allValues()
+plotting(ta,wa,area,colors)
